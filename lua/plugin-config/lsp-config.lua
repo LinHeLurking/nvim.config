95274bedf86e5df3d9e452570fa383f7029dc6bb
LinHeLurking <LinHe.Lurking@gmail.com>
2022-10-20 23:09:46 +0800

    Improve pylint

--- lua/plugin-config/lsp-config.lua
+++ lua/plugin-config/lsp-config.lua
@@ -19,9 +19,20 @@ null_ls.setup({
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
     null_ls.builtins.diagnostics.pylint.with({
-      -- Suppress docstring warning :P
-      -- Import checking is problematic for packages written in C. Disable it. Don't worry, pyright checks it right :P
-      extra_args = { "--disable=C0111,E0401" },
+      extra_args = {
+        -- Suppress docstring warning (C0111) :P
+        -- 
+        -- Import checking is problematic for packages written in C. (E0401) 
+        -- Disable it. Don't worry, pyright checks it right :P
+        --
+        -- Suppress warning about `too many locals` (R0914)
+        --
+        -- Suppress `undefined-variable` because pylint wrongly report it for some imports. (E0602)
+        -- Don't worry, pyright still handles it.
+        "--disable=C0111,E0401,R0914,E0602",
+        -- Suppress snake name check rules for 1 or 2 length names :P
+        "--good-names-rgxs=^[_a-z][_a-z0-9]?$",
+      },
     }),
   },
 })
