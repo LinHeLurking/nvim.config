027aea1b3fec32b509c06b2f6dfcfb4e1cfd9249
LinHeLurking <LinHe.Lurking@gmail.com>
2022-10-27 19:27:59 +0800

    Fix config dir path error;

--- lua/util.lua
+++ lua/util.lua
@@ -8,7 +8,7 @@ util.get_config_dir = function()
   -- Thanks https://mitchellt.com/2022/05/15/WSL-Neovim-Lua-and-the-Windows-Clipboard.html
   local in_wsl = util.is_in_wsl()
   if in_wsl then
-    return os.getenv("HOME") .. "./config/nvim"
+    return os.getenv("HOME") .. "/.config/nvim"
   else
     return "~/AppData/Local/nvim/"
   end
