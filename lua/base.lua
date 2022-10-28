710af816c082b78d042709bbab6141b8f46bce60
LinHeLurking <LinHe.Lurking@gmail.com>
2022-10-28 16:37:22 +0800

    shell opt;

--- lua/base.lua
+++ lua/base.lua
@@ -10,13 +10,13 @@ vim.opt.clipboard = "unnamedplus"
 -- Tab sizes are better managed by plugins automatically
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.o.shell = vim.fn.executable("pwsh") and "pwsh" or "powershell"
-  vim.o.shellcmdflag =
+  vim.opt.shell = vim.fn.executable("pwsh") and "pwsh" or "powershell"
+  vim.opt.shellcmdflag =
   "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-  vim.o.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-  vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-  vim.o.shellquote = ""
-  vim.o.shellxquote = ""
+  vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
 end
 
 vim.opt.incsearch = true
