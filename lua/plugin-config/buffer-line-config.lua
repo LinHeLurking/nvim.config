e5a1dfb8ee0ed453b20189c335c51afddc741f9a
LinHeLurking <LinHe.Lurking@gmail.com>
2023-01-23 12:24:34 +0800

    feat: bufferline nunmber;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- lua/plugin-config/buffer-line-config.lua
+++ lua/plugin-config/buffer-line-config.lua
@@ -3,7 +3,7 @@ vim.opt.termguicolors = true
 require("bufferline").setup({
   options = {
     mode = "buffers", -- "buffers" | "tabs". set to "tabs" to only show tabpages instead
-    numbers = "none", -- "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    numbers = "ordinal", -- "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
     close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
     right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
     left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
