4bdbb923d52339335698c1bb35c477dc132a4a77
LinHeLurking <LinHe.Lurking@gmail.com>
2023-02-10 10:15:00 +0800

    fix: dashboard project. but some problems are still there;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- lua/plugin-config/dashboard-config.lua
+++ lua/plugin-config/dashboard-config.lua
@@ -13,10 +13,9 @@ M.setup = function()
       shortcut = keymap.dashboard_shortcut,
       packages = { enable = true }, -- show how many plugins neovim loaded
       -- Limit how many projects list, action when you press key or enter it will run this action.
-      -- Keep default.
-      -- project = { limit = 8, icon = " Recent Projects", label = "", action = "Telescope find_files cwd=" },
+      -- project = { limit = 8, icon = " ", label = "Recent Projects", action = "Telescope project" },
       -- mru = { limit = 10, icon = " Recent Files", label = "" },
-      footer = { "Keep on keeping on!" }, -- footer
+      footer = { "", "Keep on keeping on!" }, -- footer
     },
   })
 end
