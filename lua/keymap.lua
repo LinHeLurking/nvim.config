f5afd268f8068f79d07f7ea39fbdd62d79a96659
LinHeLurking <LinHe.Lurking@gmail.com>
2023-03-10 15:31:28 +0800

    feat: rebind increase number for <C-i>;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -50,6 +50,7 @@ auto_bind("<C-z>", "u", opts)
 auto_bind("<C-a>", "ggVG", opts)
 -- Don't know why it cannot bind :P
 -- auto_bind("<C-S-z>", "<C-r>", opts)
+vim.keymap.set("n", "<C-i>", "<C-a>", opts)
 vim.keymap.set("v", "<C-c>", "y", opts)
 vim.keymap.set("i", "<C-h>", "<Left>", opts)
 vim.keymap.set("i", "<C-j>", "<Down>", opts)
