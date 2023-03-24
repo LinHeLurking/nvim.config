120bde61dd367309cc32250f8a3ab2e4fe3b13c4
LinHeLurking <LinHe.Lurking@gmail.com>
2023-03-24 12:48:42 +0800

    feat: better copy in windows;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- lua/plugin-config/clipboard-config.lua
+++ lua/plugin-config/clipboard-config.lua
@@ -1,15 +1,17 @@
+local util = require("util")
+
 require("osc52").setup({
   max_length = 0, -- Maximum length of selection (0 for no limit)
   silent = false, -- Disable message on successful copy
-  trim = false, -- Trim text before copy
+  trim = false,  -- Trim text before copy
 })
 
 local function osc_copy(lines, _)
   require("osc52").copy(table.concat(lines, "\n"))
 end
 
--- Thanks https://mitchellt.com/2022/05/15/WSL-Neovim-Lua-and-the-Windows-Clipboard.html
-local in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+local in_wsl = util.is_in_wsl()
+local in_windows = util.is_in_windows()
 
 if in_wsl then
   local home = os.getenv("HOME")
@@ -17,14 +19,14 @@ if in_wsl then
 
   vim.g.clipboard = {
     name = "wsl-clip",
-    copy = { ["+"] = osc_copy, ["*"] = osc_copy },
-    paste = { ["+"] = wsl_paste, ["*"] = wsl_paste },
+    copy = { ["+"] = osc_copy,["*"] = osc_copy },
+    paste = { ["+"] = wsl_paste,["*"] = wsl_paste },
   }
-else
+elseif in_windows then
   local pwsh_paste = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
   vim.g.clipboard = {
     name = "win-clip",
-    copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
-    paste = { ["+"] = pwsh_paste, ["*"] = pwsh_paste },
+    copy = { ["+"] = { "clip.exe" },["*"] = { "clip.exe" } },
+    paste = { ["+"] = pwsh_paste,["*"] = pwsh_paste },
   }
 end
