565c14f63595b3154501e1e85ea72ab7ef6f8ecd
LinHeLurking <LinHe.Lurking@gmail.com>
2023-04-19 16:14:33 +0800

    packer boot strap;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,15 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 return require("packer").startup(function(use)
   -- Packer can manage itself
   use("wbthomason/packer.nvim")
@@ -316,4 +328,10 @@ return require("packer").startup(function(use)
       require("plugin-config.nvim-surround-config")
     end,
   })
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
