21bfb06ac9df46fc07a69879a7ddbcf6e7b58d50
LinHeLurking <LinHe.Lurking@gmail.com>
2023-04-23 11:38:55 +0800

    feat: increase vertical terminal width;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- lua/plugin-config/terminal-config.lua
+++ lua/plugin-config/terminal-config.lua
@@ -6,7 +6,7 @@ require("toggleterm").setup({
     if term.direction == "horizontal" then
       return 15
     elseif term.direction == "vertical" then
-      return vim.o.columns * 0.4
+      return vim.o.columns * 0.45
     end
   end,
   direction = "float",
