a2a554e8f4918f1b3fb02a9ea4368315fb82fb61
LinHe <LinHe.Lurking@gmail.com>
2024-02-01 23:19:10 +0800

    fix: telescope version tag;
    Signed-off-by: LinHe <LinHe.Lurking@gmail.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -249,7 +249,7 @@ return require("packer").startup(function(use)
   -- Telescope
   use({
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.0",
+    branch = "0.1.x",
     requires = {
       "nvim-lua/plenary.nvim",
     },
