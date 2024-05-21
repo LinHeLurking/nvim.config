02854151b7512f65df08267b458270a55af47c2e
LinHe <LinHe.Lurking@gmail.com>
2024-05-21 14:38:19 +0800

    feat: leap highlight;
    Signed-off-by: LinHe <LinHe.Lurking@gmail.com>

--- lua/plugin-config/tokyonight-config.lua
+++ lua/plugin-config/tokyonight-config.lua
@@ -10,9 +10,9 @@ M.setup = function()
   require("tokyonight").setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-    light_style = "day",    -- The theme is used when the background is set to light
-    transparent = true,     -- Enable this to disable setting the background color
+    style = "moon",       -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    light_style = "day",  -- The theme is used when the background is set to light
+    transparent = true,   -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
       -- Style to be applied to different syntax groups
@@ -22,20 +22,20 @@ M.setup = function()
       functions = {},
       variables = {},
       -- Background styles. Can be "dark", "transparent" or "normal"
-      sidebars = "transparent",       -- style for sidebars, see below
-      floats = "transparent",         -- style for floating windows
+      sidebars = "transparent",     -- style for sidebars, see below
+      floats = "transparent",       -- style for floating windows
     },
-    sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-    day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+    sidebars = { "qf", "help" },    -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+    day_brightness = 0.3,           -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = false,             -- dims inactive windows
-    lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
+    dim_inactive = false,           -- dims inactive windows
+    lualine_bold = false,           -- When `true`, section headers in the lualine theme will be bold
     --- You can override specific color groups to use other groups or a hex color
     --- function will be called with a ColorScheme table
     ---@param colors ColorScheme
     on_colors = function(colors)
       -- inactive status line
-      -- light theme will automatically reverti this color!
+      -- light theme will automatically revert this color!
       colors.bg_statusline = "#1c1c21"
     end,
     --- You can override specific highlights to use other groups or a hex color
@@ -43,6 +43,14 @@ M.setup = function()
     ---@param highlights Highlights
     ---@param colors ColorScheme
     on_highlights = function(highlights, colors)
+      highlights.LeapLabelPrimary = {
+        bg = colors.bg_highlight,
+        fg = colors.magenta2,
+      }
+      highlights.LeapLabelSecondary = {
+        bg = colors.bg_highlight,
+        fg = colors.fg,
+      }
     end,
   })
   vim.cmd([[colorscheme tokyonight]])
