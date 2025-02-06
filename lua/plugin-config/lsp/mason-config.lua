8be990df60ce27f20c9a40a772173e298cb1f51a
LinHeLurking <LinHe.Lurking@gmail.com>
2025-02-06 10:29:08 +0800

    feat: mason can use custom lsp binary
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- lua/plugin-config/lsp/mason-config.lua
+++ lua/plugin-config/lsp/mason-config.lua
@@ -4,10 +4,18 @@ M.setup = function()
     return
   end
 
+  local custom_bin = os.getenv("MASON_USE_CUSTOM_LSP_BIN") or ""
+  custom_bin = string.upper(custom_bin)
+  local mason_path_config = "prepend" -- this is default value
+  if custom_bin == "1" or custom_bin == "ON" then
+    mason_path_config = "append" -- this leads binaries from $PATH are selected over mason installed ones.
+  end
+
   require("mason").setup({
     ui = {
       border = "single",
     },
+    PATH = mason_path_config,
   })
 end
 
