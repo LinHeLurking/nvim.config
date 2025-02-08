5b5162b1a5ee8611d5729c361c829cf8578dc0be
LinHeLurking <LinHeLurking@gmail.com>
2025-02-08 15:07:21 +0800

    feat: auto indent
    Signed-off-by: LinHeLurking <LinHeLurking@gmail.com>

--- lua/plugin-config/guess-indent-config.lua
+++ lua/plugin-config/guess-indent-config.lua
@@ -4,10 +4,9 @@ M.setup = function()
     return
   end
 
-
   -- This is the default configuration
-  require('guess-indent').setup {
-    auto_cmd = true,     -- Set to false to disable automatic execution
+  require("guess-indent").setup({
+    auto_cmd = true, -- Set to false to disable automatic execution
     filetype_exclude = { -- A list of filetypes for which the auto command gets disabled
       "netrw",
       "tutor",
@@ -18,7 +17,16 @@ M.setup = function()
       "terminal",
       "prompt",
     },
-  }
+    on_tab_options = { -- A table of vim options when tabs are detected
+      ["expandtab"] = false,
+    },
+    on_space_options = { -- A table of vim options when spaces are detected
+      ["expandtab"] = true,
+      ["tabstop"] = "detected", -- If the option value is 'detected', The value is set to the automatically detected indent size.
+      ["softtabstop"] = "detected",
+      ["shiftwidth"] = "detected",
+    },
+  })
 end
 
 return M
