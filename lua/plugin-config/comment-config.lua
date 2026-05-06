aea99d6be7aea96e6cfe8fa445a81df52b579a81
LinHeLurking <LinHe.Lurking@gmail.com>
2026-05-06 11:14:38 +0800

    feat: update comment config

--- lua/plugin-config/comment-config.lua
+++ lua/plugin-config/comment-config.lua
@@ -5,23 +5,7 @@ M.setup = function()
   end
 
 
-  require("nvim_comment").setup({
-    hook = function()
-      local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-      local only_for_these =
-      { "vue", "javascript", "typescript", "tsx", "css", "scss", "php", "html", "svelte", "astro", "lua" }
-      local enable_ctx_comment = false
-      for _, v in ipairs(only_for_these) do
-        if filetype == v then
-          enable_ctx_comment = true
-          break
-        end
-      end
-      if enable_ctx_comment then
-        require("ts_context_commentstring.internal").update_commentstring()
-      end
-    end,
-  })
+  require("nvim_comment").setup({})
 
   -- Prefer // in C/C++ comments
   vim.api.nvim_create_autocmd({ "BufEnter", "BufFilePost" }, {
